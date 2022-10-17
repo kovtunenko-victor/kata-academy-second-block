55990aab9d304501ed4d32d104994bd580de83eb
Kovtunenko Victor <kovtunenko.victor@gmail.com>
2022-10-18 00:51:03 +0300

    Prepare app

---
+++ src/main/java/ru/kata/academy/kovtunenko/second/block/dao/UserDao.java
@@ -0,0 +1,5 @@
+package ru.kata.academy.kovtunenko.second.block.dao;
+
+public interface UserDao {
+
+}
---
+++ src/main/java/ru/kata/academy/kovtunenko/second/block/model/User.java
@@ -0,0 +1,88 @@
+package ru.kata.academy.kovtunenko.second.block.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "title")
+    private String title;
+
+    public User() {
+
+    }
+
+    public User(Long id, String name, String title) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        if (!id.equals(user.id)) {
+            return false;
+        }
+
+        if (!name.equals(user.name)) {
+            return false;
+        }
+
+        return title.equals(user.title);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + title.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User: Id = %s; Name = %s; Title = %s", id, name, title);
+    }
+}
