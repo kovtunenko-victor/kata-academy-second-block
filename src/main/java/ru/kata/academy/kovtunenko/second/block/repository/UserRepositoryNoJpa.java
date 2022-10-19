d9c9cbb501112f6762ccfd65db8d680a3818bc83
Kovtunenko Victor <kovtunenko.victor@gmail.com>
2022-10-19 22:25:51 +0300

    Save code

--- src/main/java/ru/kata/academy/kovtunenko/second/block/repository/UserRepositoryNoJpa.java
+++ src/main/java/ru/kata/academy/kovtunenko/second/block/repository/UserRepositoryNoJpa.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.kata.academy.kovtunenko.second.block.model.User;
 
 import javax.persistence.EntityManager;
